476f6d74f63941e6d39230c7d196471784a39c49
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-05-21 16:13:39 -0300

    created global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>World</span>
       </h1>
     </>
