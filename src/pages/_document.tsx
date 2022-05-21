a6c3e27252a47a56377ab9585c7501a4dc205e37
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-05-21 15:59:07 -0300

    added dynamic title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,6 @@ export default class MyComponent extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
