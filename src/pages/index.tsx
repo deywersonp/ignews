7f94c59e1614a78e5d3b63a7b9230896331c0875
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-05-25 20:51:11 -0300

    created hme page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,31 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import avatarImg from '/public/images/avatar.svg';
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+
+        <Image
+          src={avatarImg}
+          alt="Girl coding"
+        />
+      </main>
     </>
   )
 }
