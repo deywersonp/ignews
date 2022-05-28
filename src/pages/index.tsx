79242e1cd3b780a6131845a7b3a4f7ad756c7b10
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-05-28 15:23:51 -0300

    changed ssr to ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import avatarImg from '/public/images/avatar.svg';
@@ -42,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1L3pT7A6lF7IRuwd99Tpo7fx', {
     expand: ['product']
   })
@@ -58,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 hours
   }
 };
