3841755afc31b088dfeedaa36fb012d651b19fe6
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-06-03 21:06:51 -0300

    added save to db on nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,19 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email: user.email } }
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
