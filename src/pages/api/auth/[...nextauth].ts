5be9733bd50c791f3222419710e60b88f43abfc4
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-06-15 19:02:35 -0300

    added already exists user on db verification

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,9 +20,25 @@ export default NextAuth({
     async signIn({ user, account, profile, email, credentials }) {
       try {
         fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email: user.email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user.email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
         return true;
