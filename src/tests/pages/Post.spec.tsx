ad24658da9172c68f64a2791b2a2b4ecd728ad49
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-10-27 20:07:31 -0300

    created test for Post page

---
+++ src/tests/pages/Post.spec.tsx
@@ -0,0 +1,81 @@
+import { render, screen } from '@testing-library/react';
+import { getSession } from 'next-auth/react';
+import Post, { getServerSideProps } from '../../pages/posts/[slug]';
+import { createClient } from '../../services/prismic'
+
+jest.mock('next-auth/react');
+jest.mock('../../services/prismic');
+
+const post = {
+  slug: 'my-new-post',
+  title: 'My new post',
+  content: '<p>Post excerpt</p>',
+  updatedAt: '10 de Abril'
+}
+
+describe('Post page', () => {
+  it('renders correctly', () => {
+    render(<Post post={post} />)
+
+    expect(screen.getByText('My new post')).toBeInTheDocument();
+    expect(screen.getByText('Post excerpt')).toBeInTheDocument();
+  });
+
+  it('redirects user if no subscription is found', async () => {
+    const getSessionMocked = jest.mocked(getSession);
+
+    getSessionMocked.mockResolvedValueOnce(null);
+
+    const response = await getServerSideProps({
+      params: { slug: 'my-new-post' }
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        redirect: expect.objectContaining({
+          destination: '/',
+        })
+      })
+    )
+  });
+
+  it('loads initial data', async () => {
+    const getSessionMocked = jest.mocked(getSession);
+    const getPrismicClientMocked = jest.mocked(createClient);
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription'
+    } as any);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            { type: 'heading', text: 'My new post' }
+          ],
+          content: [
+            { type: 'paragraph', text: 'Post content', spans: [] }
+          ],
+        },
+        last_publication_date: "2022-04-22T03:00:00.000Z",
+      }),
+    } as any);
+
+    const response = await getServerSideProps({
+      params: { slug: 'my-new-post' }
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new post',
+            content: '<p>Post content</p>',
+            updatedAt: '22 de abril de 2022'
+          }
+        }
+      })
+    )
+  });
+});
