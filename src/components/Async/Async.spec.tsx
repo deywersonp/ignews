4ca8ebc4e177acaed61d2f09b2083b30264859c7
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-10-27 20:52:37 -0300

    test with Async component using waitFor

--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -1,9 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Async } from '.';
 
 test('it renders correctly', async () => {
   render(<Async />)
 
   expect(screen.getByText('Hello World')).toBeInTheDocument();
-  expect(await screen.findByText('Button', {}, { timeout: 4000 })).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(screen.getByText('Button')).toBeInTheDocument();
+  }, { timeout: 4000 });
 });
