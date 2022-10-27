cde34ff2883269bb3221dce9ca9ac72875729fcd
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-10-26 22:56:51 -0300

    created test for SignInButton component

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import { useSession } from 'next-auth/react';
+import { SignInButton } from '.';
+
+jest.mock('next-auth/react');
+
+describe('SignInButton component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({ data: null, status: "loading" });
+
+    render(<SignInButton />)
+
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
+  });
+
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: { name: "John Doe", email: "john.doe@example.com" },
+        expires: "fake-expires",
+      },
+      status: "authenticated",
+    });
+
+    render(<SignInButton />)
+
+    expect(screen.getByText(/john doe/i)).toBeInTheDocument();
+  });
+});
